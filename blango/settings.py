eab95e1fa220b04edbbc38a053a651a55770f335
Ramakrishna Rao <brkrishna@gmail.com>
2025-04-27 05:47:03 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -124,6 +124,32 @@ class Dev(Configuration):
         },
     ]
 
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "verbose": {
+                "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+                "style": "{",
+            },
+        },        
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler", 
+                "stream": "ext://sys.stdout",
+                "formatter": "verbose"
+                },
+            # "mail_admins": {
+            #     "level": "ERROR",
+            #     "class": "django.utils.log.AdminEmailHandler",
+            #     "filters": ["require_debug_false"],
+            # },                
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        }
+    }
 
     # Internationalization
     # https://docs.djangoproject.com/en/3.2/topics/i18n/
